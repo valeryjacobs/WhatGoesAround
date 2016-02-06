9892eeadc6947f0ab6b70ac6efc93988f4640f21
svandenhoven <svandenhoven@hotmail.com>
2016-02-06 15:53:28 -0800

    Final

--- src/WhatGoesAround/WhatGoesAround.Console/Program.cs
+++ src/WhatGoesAround/WhatGoesAround.Console/Program.cs
@@ -12,12 +12,27 @@ namespace ConsoleApplication1
 {
     class Program
     {
-         static void Main(string[] args)
+
+        static void Main(string[] args)
         {
+            int currentRound = 1;
 
             var hubConnection = new HubConnection("http://whatgoesaroundcomesaround.azurewebsites.net/");
+            string DeviceId = "";
+            string action = "";
+            string[] devices = new string[] { "A", "B", "C", "D" };
+            string[] actions = new string[] { "red", "blue", "both", "off" };
             //http://localhost:11615/
             //var hubConnection = new HubConnection("http://localhost:11615/");
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=whatgoesaround;AccountKey=C8SboSwUJtGB9fglsPZmRduVVGwuab/8zhYaLHQtqzgOTfDYL4kYTGPvHcADGl0VUeRDcwNXiMTZvVuT9BCsAA==");
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("GameActivities");
+            table.CreateIfNotExists();
 
             var chat = hubConnection.CreateHubProxy("WGAHub");
             chat.On<string, string>("Send", (name, message) =>
@@ -26,64 +41,56 @@ namespace ConsoleApplication1
             });
 
             chat.On<Message>("BroadCast", (message) =>
-              {
-                  if (message is WhatGoesAround.Common.BeginGameMessage)
-                  {
-                      CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=whatgoesaround;AccountKey=C8SboSwUJtGB9fglsPZmRduVVGwuab/8zhYaLHQtqzgOTfDYL4kYTGPvHcADGl0VUeRDcwNXiMTZvVuT9BCsAA==");
+            {
+                if (message is WhatGoesAround.Common.BeginGameMessage)
+                {
+                }
+            });
 
-                      // Create the table client.
-                      CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            chat.On<PushButtonCombinationMessage>("PushButtonCombination", (message) =>
+            {
+                //1 = red, 2=blue
+                //
+                GameActivity activity = new GameActivity(Guid.NewGuid().ToString(), message.PlayerName)
+                {
+                    PiName = message.DeviceId,
+                    ReflexTime = message.ReflexTime,
+                    Round = currentRound,
+                    TimeStamp = DateTime.Now,
+                    ActivityType = 1
+                };
 
-                      // Create the CloudTable object that represents the "people" table.
-                      CloudTable table = tableClient.GetTableReference("GameActivities");
-                      table.CreateIfNotExists();
+                // Create the TableOperation object that inserts the customer entity.
+                TableOperation insertOperation = TableOperation.Insert(activity);
 
-                      GameActivity activity = new GameActivity (Guid.NewGuid().ToString(),"Johny"){
-                          PiName = "A",
-                          ReflexTime = 100,
-                          Round = 0,
-                          TimeStamp = DateTime.Now,
-                          ActivityType = 1
-                      };
-
-                      // Create the TableOperation object that inserts the customer entity.
-                      TableOperation insertOperation = TableOperation.Insert(activity);
-
-                      // Execute the insert operation.
-                      table.Execute(insertOperation);
-                  }
-              });
+                // Execute the insert operation.
+                table.Execute(insertOperation);
+            });
 
             hubConnection.Start().Wait();
 
-            string[] devices = new string[] { "A", "B", "C", "D" };
-            string[] actions = new string[] { "red", "blue", "both", "off" };
+
 
             //chat.Invoke("Notify", "Console app", hubConnection.ConnectionId);
 
             Console.WriteLine("WhatGoesAround is starting. Press any key to start");
             Console.ReadLine();
 
-            int rounds = 20;
+
+            int rounds = 2000;
             Random randDevice = new Random();
-            for(int round=1; round < rounds; round++)
+            for (int round = 1; round < rounds; round++)
             {
-
-    
-
-                string DeviceId = devices[randDevice.Next(3)];
-                string action = actions[randDevice.Next(3)];
-                Console.WriteLine(string.Format("Round {0} for {1} to press {2} ", round,DeviceId,action));
+                chat.Invoke("BeginRound", new WhatGoesAround.Common.BeginRoundMessage(currentRound));
+                currentRound = round;
+                DeviceId = devices[randDevice.Next(4)];
+                action = actions[randDevice.Next(3)];
+                Console.WriteLine(string.Format("Round {0} for {1} to press {2} ", round, DeviceId, action));
                 switch (action)
                 {
-                    case "x":
-                        chat.Invoke("BroadCast", new WhatGoesAround.Common.EndRoundMessage(1));
-                        break;
-                    case "s":
-                        chat.Invoke("BroadCast", new WhatGoesAround.Common.BeginRoundMessage(new Random().Next(0,10)));
-                        break;
                     case "red":
-                        chat.Invoke("SelectPlayer", new WhatGoesAround.Common.Action(){DeviceId = DeviceId.ToUpper(), Red = true, Blue = false });
+                        chat.Invoke("SelectPlayer", new WhatGoesAround.Common.Action() { DeviceId = DeviceId.ToUpper(), Red = true, Blue = false });
+
                         break;
                     case "blue":
                         chat.Invoke("SelectPlayer", new WhatGoesAround.Common.Action() { DeviceId = DeviceId.ToUpper(), Red = false, Blue = true });
@@ -95,6 +102,7 @@ namespace ConsoleApplication1
                         chat.Invoke("SelectPlayer", new WhatGoesAround.Common.Action() { DeviceId = DeviceId.ToUpper(), Red = false, Blue = false });
                         break;
                 }
+                chat.Invoke("BeginPlaySequence", new WhatGoesAround.Common.BeginPlaySequenceMessage());
                 System.Threading.Tasks.Task.Delay(3000).Wait();
                 //chat.Invoke("Send", "Console app", msg).Wait();
             }
