1bc5809f614cd327702e68826fb8859a1830765e
neaorin <neaorin@gmail.com>
2016-02-06 11:25:06 -0800

    phone app

--- src/WhatGoesAround/WhatGoesAround.Phone/Utils/Utils.cs
+++ src/WhatGoesAround/WhatGoesAround.Phone/Utils/Utils.cs
@@ -12,20 +12,22 @@ namespace WhatGoesAround.Phone.Utils
         static Random random = new Random();
         public static void GetRandomPositions(MainViewModel viewModel, int containerWidth, int containerHeight)
         {
-            int size1 = random.Next(200) + 30;
-            int left1 = random.Next(containerWidth - 2 * size1) + size1;
-            int top1 = random.Next(containerHeight - 2 * size1) + size1;
+            int maxsize = Math.Min(containerHeight, containerWidth) / 4;
+            int minsize = 50;
+            int size1 = random.Next(minsize, maxsize);
+            int left1 = random.Next(size1, containerWidth - size1);
+            int top1 = random.Next(size1, containerHeight - size1);
 
-            int size2 = random.Next(200) + 30;
-            int left2 = left1;
-            int top2 = top1;
+            int size2 = 0;
+            int left2 = 0;
+            int top2 = 0;
             do
             {
-                size2 = random.Next(200) + 30;
-                left2 = random.Next(containerWidth - 2 * size2) + size2;
-                top2 = random.Next(containerHeight - 2 * size2) + size2;
+                size2 = random.Next(minsize, maxsize);
+                left2 = random.Next(size2, containerWidth - size2);
+                top2 = random.Next(size2, containerHeight - size2);
             }
-            while (((left1 + size1 < left2) || (top1 + size1 < top2) || (left2 + size2 < left1) || (top2 + size2 < top1)));
+            while (!((left1 + size1 < left2) || (top1 + size1 < top2) || (left2 + size2 < left1) || (top2 + size2 < top1)));
 
             viewModel.Button1.Left = left1;
             viewModel.Button1.Top = top1;
